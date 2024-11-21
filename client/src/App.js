b71f850a35530d1b5dd7e8eb58693c07689bb993
anushac14 <113647676+anushac14@users.noreply.github.com>
2024-11-21 03:40:29 -0500

    added 404 error

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,18 @@ import Playlists from './components/Playlists';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
+// 404 NotFound component
+function NotFound() {
+  return <h1>404 - Page Not Found</h1>;
+}
+
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/playlists" element={<Playlists />} />
         <Route path="/playlists/:playlistId" element={<PlaylistsDetail />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
